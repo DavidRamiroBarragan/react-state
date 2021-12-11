d7c9d6554ac8ec40b820184d262260456aaa527f
David Ramiro Barragan <dramirobarragan@gmail.com>
2021-12-11 19:20:53 +0100

    configurada redux-thunk, redux-logger, dev-tools

--- src/config/store.js
+++ src/config/store.js
@@ -1,13 +1,24 @@
-import {createStore, combineReducers} from "redux";
+import {createStore, combineReducers, applyMiddleware} from "redux";
 import productsReducer from "../products/reducers"
 import shoppingCartReducer from "../shoppingcart/reducers";
 
+import thunk from 'redux-thunk';
+import {createLogger} from 'redux-logger/src';
+import {composeWithDevTools} from 'redux-devtools-extension';
+
+
+const middlewares = [thunk];
+
+if(process.env.NODE_ENV !== "production"){
+  middlewares.push(createLogger)
+}
 
 const store = createStore(
   combineReducers({
     products: productsReducer,
     shopppingcart: shoppingCartReducer,
   }),
+  composeWithDevTools(applyMiddleware(...middlewares))
 );
 
 export default store;
--- src/products/actions/index.js
+++ src/products/actions/index.js
@@ -1,13 +1,13 @@
 import { RECEIVE_PRODUCTS, ADD_TO_CART} from '../constants';
 import {retrieveProducts} from '../repositories/ProductsRepository';
 
-export const listProducts = () => {
-  const products = retrieveProducts();
+export const listProducts = () => async (dispatch) => {
+  const products = await retrieveProducts();
 
-  return {
+  dispatch({
     type: RECEIVE_PRODUCTS,
     products,
-  }
+  })
 }
 
 export const addToCard = (product) =>{
