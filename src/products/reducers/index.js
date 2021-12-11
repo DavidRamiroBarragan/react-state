dbf68976db75f59682a3ed71e55309c2b2e2e747
David Ramiro Barragan <dramirobarragan@gmail.com>
2021-12-11 19:52:38 +0100

    configurado nuestros reducer con immer

--- src/products/reducers/index.js
+++ src/products/reducers/index.js
@@ -1,18 +1,17 @@
 import { RECEIVE_PRODUCTS} from '../constants';
+import produce from 'immer';
 
 const initialState = [];
 
-const reducer = (state = initialState, action) => {
-  switch (action.type) {
-    case RECEIVE_PRODUCTS: {
-      return action.products.reduce((result, product) => {
-        result[product.id] = product;
-        return result;
-      }, {});
-    }
-    default:
-      return state;
+const reducer = produce((draft = initialState, action) => {
+
+  if(action.type === RECEIVE_PRODUCTS){
+    return action.products.reduce((result, product) => {
+      result[product.id] = product;
+      return result;
+    }, {});
   }
-};
+      return draft;
+});
 
 export default  reducer;
--- src/shoppingcart/reducers/index.js
+++ src/shoppingcart/reducers/index.js
@@ -1,26 +1,21 @@
 import { ADD_TO_CART } from "../constants";
+import produce from 'immer';
 
 const initialState = {
   products: {},
 };
 
-const reducer = (state = initialState, action) => {
-  switch (action.type) {
-    case ADD_TO_CART:
-      const productId = action.product.id;
-      let quantity = 0;
-      if (state.products[productId]) {
-        quantity = state.products[productId];
-      }
-      quantity++;
-      return {
-        ...state,
-        products: { ...state.products, [action.product.id]: quantity },
-      };
+const reducer = produce((draft = initialState, action) => {
+  const {type, product} = action;
+  if(type === ADD_TO_CART) {
+    const productId = product.id;
 
-    default:
-      return state;
+    if(draft.products[productId]){
+      draft.products[product.id] = ++draft.products[product.id] || 1;
+    }
+
+    return draft;
   }
-};
+});
 
 export default reducer;
